301c93a34899af38f279511f7fd74bd7f2d4f97b
MULTICOMPLEX <thepromisedplanet@gmail.com>
2022-04-20 15:50:02 +0200

    update.

--- Birthday_Probability.cpp
+++ Birthday_Probability.cpp
@@ -20,6 +20,7 @@ void Birthday_Probability()
   short int birthdays[Ndays] = {};
   int successfulTrials;
   bool sharedBirthday;
+  const int Npeople_with_same_birthday = 2;
 
   mxws rng;
 
@@ -34,7 +35,7 @@ void Birthday_Probability()
       for (int j = 0; j < people; ++j) {
         // if the given birthday is shared (has more than one person)
         // then we have a shared birthday, stop checking
-        if (++birthdays[rng(Ndays-1)] > 1) {
+        if (++birthdays[rng(Ndays-1)] > Npeople_with_same_birthday - 1) {
           sharedBirthday = true;
           break;
         }
@@ -42,9 +43,11 @@ void Birthday_Probability()
       if (sharedBirthday) ++successfulTrials;
     }
 
-    std::cout << "Probability of " << people << " people in a room sharing a birthday is \t"
-      << (double(successfulTrials) / double(TRIALS)) << std::endl;
-
+    std::cout << "The chance that, in a set of " << std::setw(3) << std::left << people 
+      << " randomly chosen people, at least " << Npeople_with_same_birthday << " people"
+      << " will share a birthday is " << std::setw(9) << std::setprecision(5) << std::fixed
+      << 100 * (double(successfulTrials) / double(TRIALS)) << " %"
+      << std::endl;
   }
   auto end = std::chrono::steady_clock::now();
   std::cout << "\nTime difference = " <<
